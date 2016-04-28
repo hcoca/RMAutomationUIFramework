66b35640540eb1839a001bdfdc3f9673f6d2e009
ArielYanarico <argos.yo@gmail.com>
2016-04-28 19:01:53 -0400

    Resource Page mapped

--- Projects/RMAutomationUIFramework/src/main/java/org/fundacionjala/automation/framework/maps/admin/resource/ResourceMap.java
+++ Projects/RMAutomationUIFramework/src/main/java/org/fundacionjala/automation/framework/maps/admin/resource/ResourceMap.java
@@ -1,5 +1,8 @@
 package org.fundacionjala.automation.framework.maps.admin.resource;
 
 public class ResourceMap {
-
+	public static final String ADD_BUTTON = "//span[text()='Add']/parent::button";
+	public static final String REMOVE_BUTTON = "//span[text()='Remove']/parent::button";
+	public static final String RESOURCE_TABLE = "//div[@id='resourcesGrid']";
+	public static final String RESOURCE_NAMES = "//div[@tabindex]/div/div/child::*[3]//span";
 }
--- Projects/RMAutomationUIFramework/src/main/java/org/fundacionjala/automation/framework/pages/admin/resource/ResourcePage.java
+++ Projects/RMAutomationUIFramework/src/main/java/org/fundacionjala/automation/framework/pages/admin/resource/ResourcePage.java
@@ -1,10 +1,44 @@
 package org.fundacionjala.automation.framework.pages.admin.resource;
 
+import java.util.List;
+
+import org.fundacionjala.automation.framework.maps.admin.resource.ResourceMap;
 import org.fundacionjala.automation.framework.pages.admin.home.AdminPage;
+import org.fundacionjala.automation.framework.utils.common.BrowserManager;
+import org.fundacionjala.automation.framework.utils.common.UIActions;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class ResourcePage extends AdminPage {
 	
+	@FindBy (xpath = ResourceMap.ADD_BUTTON) WebElement addButton;
+	@FindBy (xpath = ResourceMap.REMOVE_BUTTON) WebElement removeButton;
+	@FindBy (xpath = ResourceMap.RESOURCE_TABLE) WebElement resourceTable;
+	@FindBy (xpath = ResourceMap.RESOURCE_NAMES) List<WebElement> resourceNames;
+	
 	public ResourcePage() {
+		PageFactory.initElements(BrowserManager.getDriver(), this);
 	}
-
+	
+	public AddResourcePage clickOnAddButton(){
+		UIActions.waitFor(ResourceMap.ADD_BUTTON);
+		UIActions.clickAt(addButton);
+		return new AddResourcePage();
+	}
+	
+	public RemoveResourcePage clickOnRemoveButton(){
+		UIActions.waitFor(ResourceMap.REMOVE_BUTTON);
+		UIActions.clickAt(removeButton);
+		return new RemoveResourcePage();
+	}
+	
+	public boolean verifyResourceExist(String resourceName){
+		for (WebElement name : resourceNames) {
+			if (name.getText().equalsIgnoreCase(resourceName)) {
+				return true;
+			}
+		}		
+		return false;
+	}
 }
