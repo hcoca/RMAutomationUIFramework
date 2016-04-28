777e819cf713b40920bac4156bf3035da6db6856
SamuelSahonero <ssahonero@gmail.com>
2016-04-28 18:05:19 -0400

    Updated Admin, Login, Remove Resource Page and Map classes

--- Projects/RMAutomationUIFramework/src/main/java/org/fundacionjala/automation/framework/pages/admin/login/LoginPage.java
+++ Projects/RMAutomationUIFramework/src/main/java/org/fundacionjala/automation/framework/pages/admin/login/LoginPage.java
@@ -10,11 +10,27 @@ import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
 
+	@FindBy (xpath = LoginMap.USER_NAME_TEXT_FIELD) WebElement userNameTextField;
+	@FindBy (xpath = LoginMap.PASSWORD_TEXT_FIELD) WebElement passwordTextField;
 	@FindBy (xpath = LoginMap.SIGN_IN_BUTTON) WebElement sigInButton;
 	
 	public LoginPage() {
 		BrowserManager.setUrl("http://172.20.208.84:4040/admin/#/login");
 		PageFactory.initElements(BrowserManager.getDriver(), this);
+	}
+	
+	public LoginPage setUserName(String userName) {
+		UIActions.waitFor(LoginMap.USER_NAME_TEXT_FIELD);
+		UIActions.typeOn(userNameTextField, userName);
+		
+		return this;
+	}
+	
+	public LoginPage setPassword(String password) {
+		UIActions.waitFor(LoginMap.PASSWORD_TEXT_FIELD);
+		UIActions.typeOn(passwordTextField, password);
+		
+		return this;
 	}
 	
 	public AdminPage clickSigInButton()	{
--- Projects/RMAutomationUIFramework/src/main/java/org/fundacionjala/automation/framework/pages/admin/navigation/LeftMenu.java
+++ Projects/RMAutomationUIFramework/src/main/java/org/fundacionjala/automation/framework/pages/admin/navigation/LeftMenu.java
@@ -1,5 +1,25 @@
 package org.fundacionjala.automation.framework.pages.admin.navigation;
 
-public class LeftMenu {
+import org.fundacionjala.automation.framework.maps.admin.navigation.LeftMenuMap;
+import org.fundacionjala.automation.framework.pages.admin.resource.ResourcePage;
+import org.fundacionjala.automation.framework.utils.common.BrowserManager;
+import org.fundacionjala.automation.framework.utils.common.UIActions;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+public class LeftMenu {
+	
+	@FindBy (xpath = LeftMenuMap.RESOURCES_BUTTON) WebElement resourcesButton;
+	
+	public LeftMenu() {
+		PageFactory.initElements(BrowserManager.getDriver(), this);
+	}
+	
+	public ResourcePage clickResourcesButton() {
+		UIActions.waitFor(LeftMenuMap.RESOURCES_BUTTON);
+		UIActions.clickAt(resourcesButton);
+		
+		return new ResourcePage();
+	}
 }
