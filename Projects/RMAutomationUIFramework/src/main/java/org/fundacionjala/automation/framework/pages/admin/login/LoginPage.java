7b02fde9e1fba2f743801793efa521735857bfb9
SamuelSahonero <ssahonero@gmail.com>
2016-04-28 18:55:31 -0400

    Login Page Browser Manager URL updated

--- Projects/RMAutomationUIFramework/src/main/java/org/fundacionjala/automation/framework/pages/admin/login/LoginPage.java
+++ Projects/RMAutomationUIFramework/src/main/java/org/fundacionjala/automation/framework/pages/admin/login/LoginPage.java
@@ -3,6 +3,7 @@ package org.fundacionjala.automation.framework.pages.admin.login;
 import org.fundacionjala.automation.framework.maps.admin.login.LoginMap;
 import org.fundacionjala.automation.framework.pages.admin.home.AdminPage;
 import org.fundacionjala.automation.framework.utils.common.BrowserManager;
+import org.fundacionjala.automation.framework.utils.common.PropertiesReader;
 import org.fundacionjala.automation.framework.utils.common.UIActions;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -15,7 +16,7 @@ public class LoginPage {
 	@FindBy (xpath = LoginMap.SIGN_IN_BUTTON) WebElement sigInButton;
 	
 	public LoginPage() {
-		BrowserManager.setUrl("http://172.20.208.84:4040/admin/#/login");
+		BrowserManager.setUrl(PropertiesReader.getAdminURL());
 		PageFactory.initElements(BrowserManager.getDriver(), this);
 	}
 	
