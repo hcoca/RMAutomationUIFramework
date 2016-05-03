889157c97d3b9d4bc61011361c69eeb264816131
Fernando Gutierrez <fernandogutierrez279@gmail.com>
2016-05-03 14:47:57 -0400

    runner for run test cases in serenety was modified

--- Projects/RMAutomationUIFramework/src/test/java/org/fundacionjala/automation/scenario/ResourceRunnerTest.java
+++ Projects/RMAutomationUIFramework/src/test/java/org/fundacionjala/automation/scenario/ResourceRunnerTest.java
@@ -1,13 +1,13 @@
 package org.fundacionjala.automation.scenario;
 
 
+import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(features ="src/test/resources/features/admin/resource/") 
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(features ="src/test/resources/features/admin/resource") 
 
 public class ResourceRunnerTest extends AbstractTestNGCucumberTests{
 	              
