9d73e9bf1e0c14758daea88bf6edbc333a76be92
Fernando Gutierrez <fernandogutierrez279@gmail.com>
2016-05-06 16:40:26 -0400

    Maps for conference rooms and resources associations was added

---
+++ Projects/RMAutomationUIFramework/src/main/java/org/fundacionjala/automation/framework/maps/admin/conferencerooms/ConferenceRoomsMap.java
@@ -0,0 +1,10 @@
+package org.fundacionjala.automation.framework.maps.admin.conferencerooms;
+
+public class ConferenceRoomsMap {
+
+	public static final String ROOMS_ROWS = "//div[@class='ngCanvas']/*/div[3]";
+	public static final String ENABLED_ROOMS_ROWS = "//span[@class='text-glow-green fa fa-power-off']";
+	public static final String DISABLED_ROOMS_ROWS = "//span[@class='text-glow-gray fa fa-power-off']";
+	
+	
+}
---
+++ Projects/RMAutomationUIFramework/src/main/java/org/fundacionjala/automation/framework/maps/admin/conferencerooms/ResourceAssociationsMap.java
@@ -0,0 +1,24 @@
+package org.fundacionjala.automation.framework.maps.admin.conferencerooms;
+
+public class ResourceAssociationsMap {
+
+	public static final String RESOURCES_AVAILABLES 
+	                     = "//div[@class='list-group']/div[@ng-repeat='availableResource in availableResources']/div[2]/span";
+	
+	public static final String PLUSICONS_BUTTON 
+	                     = "//div[@class='list-group']/div[@ng-repeat='availableResource in availableResources']/div[3]/*";
+	
+	public static final String ASSOCIATEDRESOURCES_ROWS 
+	                     = "//div[@class='list-group']/div[@ng-repeat='associatedResource in roomResources']/div[2]/*";
+	
+	public static final String ASSOCIATEDRESOURCES_QUANTITY_ROWS
+	                     = "//div[@class='list-group']/div[@ng-repeat='associatedResource in roomResources']/div[3]/*";
+	
+	public static final String MINUSICONS_BUTTON 
+	                     = "//div[@class='list-group']/div[@ng-repeat='associatedResource in roomResources']/div[4]/button/*";
+
+	public static final String SAVE_BUTTON 
+	                     = "//button[@class='info']";
+	
+
+}
