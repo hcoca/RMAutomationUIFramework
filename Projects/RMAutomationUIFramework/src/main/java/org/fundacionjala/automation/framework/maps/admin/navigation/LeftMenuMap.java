65199c088e9148ffbd80e0acea35bedc07ed6d01
Fernando Gutierrez <fernandogutierrez279@gmail.com>
2016-05-06 17:24:17 -0400

    Button for conference rooms to left menu was added

--- Projects/RMAutomationUIFramework/src/main/java/org/fundacionjala/automation/framework/maps/admin/navigation/LeftMenuMap.java
+++ Projects/RMAutomationUIFramework/src/main/java/org/fundacionjala/automation/framework/maps/admin/navigation/LeftMenuMap.java
@@ -2,5 +2,7 @@ package org.fundacionjala.automation.framework.maps.admin.navigation;
 
 public class LeftMenuMap {
 	public static final String RESOURCES_BUTTON = "//a[@class='clickeable ng-binding'][text()='Resources']";
+	public static final String CONFERENCE_ROOMS_BUTTON = "Conference Rooms";
+	
 	public static final String ISSUES_BUTTON = "//a[text()='Issues']";
 }
--- Projects/RMAutomationUIFramework/src/main/java/org/fundacionjala/automation/framework/pages/admin/navigation/LeftMenu.java
+++ Projects/RMAutomationUIFramework/src/main/java/org/fundacionjala/automation/framework/pages/admin/navigation/LeftMenu.java
@@ -1,11 +1,13 @@
 package org.fundacionjala.automation.framework.pages.admin.navigation;
 
 import org.fundacionjala.automation.framework.maps.admin.navigation.LeftMenuMap;
+import org.fundacionjala.automation.framework.pages.admin.conferencerooms.ConferenceRoomsPage;
 import org.fundacionjala.automation.framework.pages.admin.resource.ResourcePage;
 import org.fundacionjala.automation.framework.utils.common.BrowserManager;
 import org.fundacionjala.automation.framework.utils.common.LogManager;
 import org.fundacionjala.automation.framework.utils.common.UIActions;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.server.handler.SwitchToWindow;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
@@ -13,6 +15,9 @@ public class LeftMenu {
 	
 	@FindBy (xpath = LeftMenuMap.RESOURCES_BUTTON) WebElement resourcesButton;
 	@FindBy (xpath = LeftMenuMap.ISSUES_BUTTON) WebElement issuesButton;
+	@FindBy (linkText = LeftMenuMap.CONFERENCE_ROOMS_BUTTON) WebElement conferenceRoomsButton;
+	
+	
 	public LeftMenu() {
 		PageFactory.initElements(BrowserManager.getDriver(), this);
 	}
@@ -24,7 +29,13 @@ public class LeftMenu {
 		
 		return new ResourcePage();
 	}
-
+	public ConferenceRoomsPage clickOnConferenceRoomsButton() throws InterruptedException{
+		LogManager.info("Click on conference rooms page");
+		UIActions.clickAt(conferenceRoomsButton);
+		conferenceRoomsButton.click();
+		return new ConferenceRoomsPage();
+	}
+	
 	public LeftMenu clickOnIssuesButton() {
 		UIActions.waitFor(LeftMenuMap.ISSUES_BUTTON);
 		UIActions.clickAt(issuesButton);
