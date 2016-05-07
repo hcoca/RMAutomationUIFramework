10a2d0031c992cf1412e43bb55f018f764e4de50
belenluz <Lucero.Penarrieta@fundacion-jala.org>
2016-05-07 16:50:38 -0400

    Out Of Order scenario has been updated

--- Projects/RMAutomationUIFramework/src/test/java/org/fundacionjala/automation/scenario/steps/admin/conferenceRoom/AvailableOutOfOrderSteps.java
+++ Projects/RMAutomationUIFramework/src/test/java/org/fundacionjala/automation/scenario/steps/admin/conferenceRoom/AvailableOutOfOrderSteps.java
@@ -10,6 +10,9 @@ import org.fundacionjala.automation.framework.pages.tablet.settings.NavigationPa
 import org.fundacionjala.automation.framework.utils.api.managers.OutOfOrderAPIManager;
 import org.fundacionjala.automation.framework.utils.api.objects.admin.OutOfOrder;
 import org.fundacionjala.automation.framework.utils.common.BrowserManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -18,13 +21,7 @@ import cucumber.api.java.en.When;
 public class AvailableOutOfOrderSteps {
 	AdminPage home;
 	ConferenceRoomsPage room;
-	
-	@Given("^I create an Out of Order on a specific \"([^\"]*)\" room$")
-	public void i_create_an_Out_of_Order_on_a_specific_room(String arg1) throws Throwable {
-		/*OutOfOrder outOfOrder = new OutOfOrder("2016-06-06T02:30:00.000Z", "2016-06-06T02:35:00.000Z", "572bd5092af305ac2ee05802", "Temporarily Out of Order", false);
-		OutOfOrderAPIManager.postRequest(
-		"http://172.20.208.84:4040/services/572d083bd63d1e940e6baf93/rooms/572d083bd63d1e940e6baf9c/out-of-orders?active=false&email=true", outOfOrder);*/
-	} 
+	String roomName = "Room09";
 	
 	@Given("^I logged to Room Manager Admin$")
 	public void i_logged_to_Room_Manager_Admin() throws Throwable {
@@ -35,11 +32,21 @@ public class AvailableOutOfOrderSteps {
 					.clickOnSigInButton()
 					.refreshPage();
 	}
+	
+	@Given("^I create an Out of Order on a specific \"([^\"]*)\" room$")
+	public void i_create_an_Out_of_Order_on_a_specific_room(String arg1) throws Throwable {
+		room = home.leftMenu.clickOnConferenceRoomsButton()
+					.openConfigurationPage(roomName)
+					.clickOnOutOfOrder()
+					.clickOnBoxButon()
+					.ClickOnClosedForMaintenanceLink()
+					.clickOnSave();
+	} 
 
 	@When("^I did click on the icon of Out of Order$")
 	public void i_did_click_on_the_icon_of_Out_of_Order() throws Throwable {
 		room = home.leftMenu.clickOnConferenceRoomsButton()
-				   			.selectOutOfOrderIcon();
+				   			.selectOutOfOrderIcon(roomName);
 	}
 	
 	@When("^I sign in to Tablet page using the \"([^\"]*)\" room$")
@@ -49,10 +56,10 @@ public class AvailableOutOfOrderSteps {
 				    	.setUpServiceURL("http://172.20.208.84:4040/")
 				    	.clickOnSaveButton()
 				    	.clickOnNavigationButton();
-	    	
+		
 	    HomePage home =	navigation
 				    	.clickOnRoomToggleButton()
-				    	.selectConferenceRoom("Room01")
+				    	.selectConferenceRoom(roomName)
 				    	.clickOnSaveButton()
 				    	.topMenu
 				    	.clickOnHomeButton();
@@ -60,7 +67,12 @@ public class AvailableOutOfOrderSteps {
 	
 	@Then("^I validate if the \"([^\"]*)\" room has changed its status to non-available$")
 	public void i_validate_if_the_room_has_changed_its_status_to_non_available(String arg1) throws Throwable {
-	    
+		boolean verification = false;
+		WebElement title =BrowserManager.getDriver().findElement(By.xpath("//div[@ng-bind='next._title']"));
+	    if(title.getText().contains("Closed for maintenance")){
+	    	verification = true;
+	    }
+	    Assert.assertTrue(verification);
 	}
 	
 }
