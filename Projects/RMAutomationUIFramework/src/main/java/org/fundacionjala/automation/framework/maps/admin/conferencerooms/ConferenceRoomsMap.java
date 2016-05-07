3a0ae9a771667d9e298056d68440b3f4df37471d
belenluz <Lucero.Penarrieta@fundacion-jala.org>
2016-05-07 16:49:06 -0400

    ConferenceRoomsMap has been updated

--- Projects/RMAutomationUIFramework/src/main/java/org/fundacionjala/automation/framework/maps/admin/conferencerooms/ConferenceRoomsMap.java
+++ Projects/RMAutomationUIFramework/src/main/java/org/fundacionjala/automation/framework/maps/admin/conferencerooms/ConferenceRoomsMap.java
@@ -4,7 +4,7 @@ public class ConferenceRoomsMap {
 
 	public static final String ENABLED_ROOMS_ROWS = "//span[@class='text-glow-green fa fa-power-off']";
 	public static final String DISABLED_ROOMS_ROWS = "//span[@class='text-glow-gray fa fa-power-off']";
-	public static final String OUT_OF_ORDER_ICONS= "//span[text()='Room08']/preceding::span[ @class='fa fa-calendar ng-scope text-glow-gray']";
+	public static final String OUT_OF_ORDER_ICONS= "//span[text()='roomName']/preceding::span[ @class='fa fa-calendar ng-scope text-glow-gray']";
 	public static final String ROOMS_COLUMN = "//div[@class='ngCanvas']/*/div[3]//span[2]";
 	
 }
