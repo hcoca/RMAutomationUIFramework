93368ef35ab7783056bf9119d03aea0a0654989c
Maria <maria.alcocer@fundacion-jala.org>
2016-05-07 19:09:02 -0400

    added'association is displayey when remove button is clicked'scenario

--- Projects/RMAutomationUIFramework/src/main/java/org/fundacionjala/automation/framework/pages/admin/conferencerooms/ResourceAssociationsPage.java
+++ Projects/RMAutomationUIFramework/src/main/java/org/fundacionjala/automation/framework/pages/admin/conferencerooms/ResourceAssociationsPage.java
@@ -24,6 +24,7 @@ public class ResourceAssociationsPage {
 	private List<WebElement> resourcesAvailables;
 	private List<WebElement> getResourcesAvailabes()
 	{
+		
 	   return resourcesAvailables;
 	}
 	
@@ -65,7 +66,9 @@ public class ResourceAssociationsPage {
 	}
 	private void addResourceByPos(int pos)
 	{
+		UIActions.waitFor("//div[@class='list-group']/div[@ng-repeat='availableResource in availableResources']/div[3]/*");
 		WebElement btnPlusOfResource = getPlusIcons().get(pos);
+		
 		btnPlusOfResource.click();
 	}
 
