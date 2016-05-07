00f9b41ace3ee75728f7d5daaf03df416d93e747
belenluz <Lucero.Penarrieta@fundacion-jala.org>
2016-05-07 15:08:46 -0400

    OutOfOrderMap updated

---
+++ Projects/RMAutomationUIFramework/src/main/java/org/fundacionjala/automation/framework/maps/admin/conferencerooms/OutOfOrderMap.java
@@ -0,0 +1,11 @@
+package org.fundacionjala.automation.framework.maps.admin.conferencerooms;
+
+public class OutOfOrderMap {
+
+	public static final String FROM_FIELD = "(//input[@type='text'])[4]";
+	public static final String TO_FIELD = "(//input[@type='text'])[7]";
+	public static final String ACTIVE_BUTTON = "//div[2]/div/div/div/label/span";
+	public static final String BOX_BUTTON = "span.fa.fa-caret-down";
+	public static final String CLOSED_FOR_MAINTENANCE_LINK = "Closed for maintenance";
+	public static final String SAVE_BUTTON = "//button[@class='info']";
+}
