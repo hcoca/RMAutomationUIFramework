c21337c50aeb68653cb69bb64e2c5f64b38718a2
Fernando Gutierrez <fernandogutierrez279@gmail.com>
2016-05-09 14:00:17 -0400

    Two new methods added

--- Projects/RMAutomationUIFramework/src/main/java/org/fundacionjala/automation/framework/pages/admin/conferencerooms/ConferenceRoomsPage.java
+++ Projects/RMAutomationUIFramework/src/main/java/org/fundacionjala/automation/framework/pages/admin/conferencerooms/ConferenceRoomsPage.java
@@ -5,11 +5,15 @@ import java.util.List;
 import org.fundacionjala.automation.framework.maps.admin.conferencerooms.ConferenceRoomsMap;
 import org.fundacionjala.automation.framework.utils.common.BrowserManager;
 import org.fundacionjala.automation.framework.utils.common.ExplicitWait;
+import org.fundacionjala.automation.framework.utils.common.LogManager;
 import org.fundacionjala.automation.framework.utils.common.UIActions;
 import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ConferenceRoomsPage {
    
@@ -52,6 +56,7 @@ public class ConferenceRoomsPage {
 	
     public RoomInfoPage openConfigurationPage(String roomToModify) {
 	    UIActions.doubleClick(getRoom(roomToModify));
+    	LogManager.info("Open pop-up configuration of the room: " + roomToModify);
 		return new RoomInfoPage();
 	}
     
@@ -59,6 +64,8 @@ public class ConferenceRoomsPage {
     	WebElement roomElement = getRoom(roomToModify);
     	roomElement.click();
     	UIActions.doubleClickJS(roomElement);
+
+    	LogManager.info("Double click on the room " + roomToModify);
 		return new RoomInfoPage();
 	}
     
@@ -75,11 +82,14 @@ public class ConferenceRoomsPage {
 		return true;
 	}
 	
-	public boolean VerifyIfRoomExist(String expectedResult) {
-		if(getRoom(expectedResult) != null)
+	public boolean VerifyIfRoomExist(String room) {
+		if(getRoom(room) != null)
 		{
+	    	LogManager.info("Verified the existence of the room: " + room);
 			return true;
 		}else{
+
+	    	LogManager.info("The room was not found " + room);
 			return false;
 		}
 	}
@@ -88,19 +98,51 @@ public class ConferenceRoomsPage {
 	private WebElement getResource(String resourceName){
 		for (WebElement resource : resourceButtons) {
 			if(resource.getText().trim().equalsIgnoreCase(resourceName)){
+
+		    	LogManager.info("Resource found" + resourceName);
 				return resource;
 			}
 		}
+
+    	LogManager.info("Resource was not found " + resourceName);
 		return null;
 	}
 	
 	
-	public boolean verifyIfResourceCreatedIsInConferenceRoomPage(
-			String expectedResult) {
+	public boolean verifyIfResourceCreatedIsInConferenceRoomPage(String expectedResult) {
 		if(getResource(expectedResult) != null){
 			return true;
 		}
 		return false;
 		
 	}
+
+	public ConferenceRoomsPage clickOnResource(String resourceName) {
+		
+		String stringXpath = ConferenceRoomsMap.RESOURCES.replace("resource", resourceName);
+		ExplicitWait.clickWhenReady(By.xpath(stringXpath), 10);
+		return this;
+	}
+
+	public boolean isQuantityDisplayed(String quantity) {
+		
+		String stringXpath = ConferenceRoomsMap.RESOURCES_QUANTITY.replace("qty", quantity);
+
+		WebDriverWait wait = new WebDriverWait(BrowserManager.getDriver(),5);
+
+		try {
+			wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(stringXpath)));
+			return true;
+			
+		} catch (TimeoutException te) {
+			
+			return false;
+		}	
+	}
 }
+
+
+
+
+
+
