5c87d37dcee04a2a58fa3df06347805d7d4f7402
belenluz <Lucero.Penarrieta@fundacion-jala.org>
2016-05-11 19:38:47 -0400

    Fix Framework: OutOfOrderMap-HomeMap

--- Projects/RMAutomationUIFramework/src/main/java/org/fundacionjala/automation/framework/maps/tablet/home/HomeMap.java
+++ Projects/RMAutomationUIFramework/src/main/java/org/fundacionjala/automation/framework/maps/tablet/home/HomeMap.java
@@ -4,4 +4,5 @@ public class HomeMap {
 
 	public static final String SCHEDULE_BUTTON = "//rm-panel-option[@class='tile-column-option tile-column-option-landscape']/descendant::div[@href='#/schedule'][@ui-sref='roommanager.room.schedule']";
 	public static final String TITLE_OUT_OF_ORDER = "//div[@ng-bind='next._title']"; 
+	public static final String TIME_OUT_OF_ORDER = "//div[@class='time']/div";
 }
