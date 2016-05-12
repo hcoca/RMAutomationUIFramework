dfe987bd02e05b3d7eda6eeddd8fbcdc89b791af
Maria <maria.alcocer@fundacion-jala.org>
2016-05-11 21:26:39 -0400

    Init Framework: AddResourcePage, RemoveResourcePage, ResourceInfoPage documented and reviewed java codeconventions

--- Projects/RMAutomationUIFramework/src/main/java/org/fundacionjala/automation/framework/pages/admin/resource/RemoveResourcePage.java
+++ Projects/RMAutomationUIFramework/src/main/java/org/fundacionjala/automation/framework/pages/admin/resource/RemoveResourcePage.java
@@ -8,16 +8,31 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+/**
+ * This class represents RemoveResourcePage
+ * @author mariaalcocer
+ *
+ */
 public class RemoveResourcePage {
 
-	@FindBy (xpath = RemoveResourceMap.REMOVE_BUTTON) WebElement removeButton;
-	@FindBy (xpath = RemoveResourceMap.CANCEL_BUTTON) WebElement cancelButton;
-	@FindBy (xpath = RemoveResourceMap.CLOSE_BUTTON) WebElement closeButton;
+	@FindBy (xpath = RemoveResourceMap.REMOVE_BUTTON) 
+	WebElement removeButton;
+	@FindBy (xpath = RemoveResourceMap.CANCEL_BUTTON) 
+	WebElement cancelButton;
+	@FindBy (xpath = RemoveResourceMap.CLOSE_BUTTON) 
+	WebElement closeButton;
 	
+	/**
+	 * The constructor initialize the RemoveResourcePage
+	 */
 	public RemoveResourcePage() {
 		PageFactory.initElements(BrowserManager.getDriver(), this);
 	}
 	
+	/**
+	 * This function is to click on REMOVE button
+	 * @return ResourcePage
+	 */
 	public ResourcePage clickOnRemoveButton() {
 		UIActions.waitFor(RemoveResourceMap.REMOVE_BUTTON);
 		UIActions.clickAt(removeButton);
@@ -25,6 +40,10 @@ public class RemoveResourcePage {
 		return new ResourcePage();
 	}
 	
+	/**
+	 * This function is to click on CANCEL button
+	 * @return ResourcePage
+	 */
 	public ResourcePage clickOnCancelButton() {
 		UIActions.waitFor(RemoveResourceMap.CANCEL_BUTTON);
 		UIActions.clickAt(cancelButton);
@@ -32,20 +51,28 @@ public class RemoveResourcePage {
 		return new ResourcePage();
 	}
 	
+	/**
+	 * This function is to click on CLOSE button
+	 * @return ResourcePage
+	 */
 	public ResourcePage clickOnCloseButton() {
 		UIActions.waitFor(RemoveResourceMap.CLOSE_BUTTON);
 		UIActions.clickAt(closeButton);
 		
 		return new ResourcePage();
 	}
-
+	
+	/**
+	 * This function is to verify if an association is displayed
+	 * @param roomName
+	 * @return true if the association id displayed else false
+	 */
 	public boolean verifyAssociatedRoomExist(String roomName) {
 		try {
-			String xpath = RemoveResourceMap.NAME_ROOM.replace("roomName", roomName);
-			if(BrowserManager.getDriver().findElement(By.xpath(xpath))!= null)
-				return true;
-			else
-				return false;
+			String xpath = RemoveResourceMap.NAME_ROOM.replace(
+				"roomName", roomName);
+			return (BrowserManager.getDriver().findElement(
+				By.xpath(xpath))!= null) ? true : false;
 		} catch (Exception e) {
 			return false;
 		}
--- Projects/RMAutomationUIFramework/src/main/java/org/fundacionjala/automation/framework/pages/admin/resource/ResourceInfoPage.java
+++ Projects/RMAutomationUIFramework/src/main/java/org/fundacionjala/automation/framework/pages/admin/resource/ResourceInfoPage.java
@@ -12,22 +12,45 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+/**
+ * This class represents ResourceInfoPage
+ * @author mariaalcocer
+ *
+ */
 public class ResourceInfoPage {
-	@FindBy (xpath = ResourceInfoMap.NAME_TEXT_FIELD) WebElement nameTextField;
-	@FindBy (xpath = ResourceInfoMap.DISPLAY_NAME_TEXT_FIELD) WebElement displayNameTextField;
-	@FindBy (xpath = ResourceInfoMap.DESCRIPTION_TEXT_FIELD) WebElement descriptionTextField;
-	@FindBy (xpath = ResourceInfoMap.SAVE_BUTTON) WebElement saveButton;
-	@FindBy (xpath = ResourceInfoMap.CANCEL_BUTTON) WebElement cancelButton;
-	@FindBy (xpath = ResourceInfoMap.ICON_BUTTON) WebElement iconButton;
-	@FindBy (xpath = ResourceInfoMap.LEFT_BUTTON) WebElement leftButton;
-	@FindBy (xpath = ResourceInfoMap.RIGHT_BUTTON) WebElement rightButton;
-	@FindBy (css = ResourceInfoMap.PAGE_COUNT) WebElement txt_count;
+    
+	@FindBy (xpath = ResourceInfoMap.NAME_TEXT_FIELD) 
+	WebElement nameTextField;
+	@FindBy (xpath = ResourceInfoMap.DISPLAY_NAME_TEXT_FIELD)
+	WebElement displayNameTextField;
+	@FindBy (xpath = ResourceInfoMap.DESCRIPTION_TEXT_FIELD) 
+	WebElement descriptionTextField;
+	@FindBy (xpath = ResourceInfoMap.SAVE_BUTTON) 
+	WebElement saveButton;
+	@FindBy (xpath = ResourceInfoMap.CANCEL_BUTTON) 
+	WebElement cancelButton;
+	@FindBy (xpath = ResourceInfoMap.ICON_BUTTON) 
+	WebElement iconButton;
+	@FindBy (xpath = ResourceInfoMap.LEFT_BUTTON) 
+	WebElement leftButton;
+	@FindBy (xpath = ResourceInfoMap.RIGHT_BUTTON) 
+	WebElement rightButton;
+	@FindBy (css = ResourceInfoMap.PAGE_COUNT) 
+	WebElement txt_count;
 	
+	/**
+	 * The constructor initialize the ResourceInfoPage class
+	 */
 	public ResourceInfoPage()
 	{
 		PageFactory.initElements(BrowserManager.getDriver(), this);
 	}
 	
+	/**
+	 * Fills name text field on ResourceInfoPage
+	 * @param name
+	 * @return ResourceInfoPage
+	 */
 	public ResourceInfoPage setResourceName(String name)
 	{
 		UIActions.waitFor(ResourceInfoMap.NAME_TEXT_FIELD);
@@ -36,6 +59,11 @@ public class ResourceInfoPage {
 		return this;
 	}
 	
+	/**
+	 * Fills displayName text field on ResourceInfoPage
+	 * @param displayName
+	 * @return ResourceInfoPage
+	 */
 	public ResourceInfoPage setDisplayName(String displayName)
 	{
 		UIActions.waitFor(ResourceInfoMap.DISPLAY_NAME_TEXT_FIELD);
@@ -44,6 +72,11 @@ public class ResourceInfoPage {
 		return this;
 	}
 	
+	/**
+	 * Fills description text field on ResourceInfoPage
+	 * @param description
+	 * @return ResourceInfoPage
+	 */
 	public ResourceInfoPage setDescription(String description)
 	{
 		UIActions.waitFor(ResourceInfoMap.DESCRIPTION_TEXT_FIELD);
@@ -52,20 +85,28 @@ public class ResourceInfoPage {
 		return this;
 	}
 	
+	/**
+	 * Selects an icon on ResourceInfoPage
+	 * @param icon
+	 * @return ResourceInfoPage
+	 */
 	public ResourceInfoPage selectIcon(String icon)
 	{	
 		
 		UIActions.waitFor(ResourceInfoMap.ICON_BUTTON);
 		UIActions.clickAt(iconButton);
 		WebElement iconToChange = FindIcon(icon);
-		if (iconToChange != null)
-		{
+		if (iconToChange != null){
 			iconToChange.click();
 		}
 		
 		return this;
 	}
 	
+	/**
+	 * This function is to click on SAVE button on ResourceInfoPage
+	 * @return ResourceInfoPage
+	 */
 	public ResourcePage clickOnSaveButton()
 	{
 		UIActions.waitFor(ResourceInfoMap.SAVE_BUTTON);
@@ -74,6 +115,10 @@ public class ResourceInfoPage {
 		return new ResourcePage();
 	}
 	
+	/**
+	 * This function is to click on CANCEL button on ResourceInfoPage
+	 * @return ResourceInfoPage
+	 */
 	public ResourcePage clickOnCancelButton()
 	{
 		UIActions.waitFor(ResourceInfoMap.CANCEL_BUTTON);
@@ -82,9 +127,15 @@ public class ResourceInfoPage {
 		return new ResourcePage();
 	}
 	
+	/**
+	 * This function is to select an ICON button on ResourceInfoPage
+	 * @param iconName
+	 * @return ResourceInfoPage
+	 */
 	public WebElement FindIcon(String iconName)
 	{
-		String xpath = ResourceInfoMap.ICON.replace("iconName", iconName);
+		String xpath = 
+			ResourceInfoMap.ICON.replace("iconName", iconName);
 		List<WebElement> elements;
 		LogManager.info("Finding the icon from page 1:" + iconName);
 		
@@ -94,27 +145,38 @@ public class ResourceInfoPage {
 		}
 		
 		byte count = 1;
-		elements = BrowserManager.getDriver().findElements(By.xpath(xpath));
+		elements = 
+			BrowserManager.getDriver().findElements(By.xpath(xpath));
 		while(elements.size() == 0 && count <= 9)
 		{
-			LogManager.info(iconName + " is not in the page " + count); 
+			LogManager.info(iconName 
+					+ " is not in the page " 
+					+ count); 
 			rightButton.click();
-			elements = BrowserManager.getDriver().findElements(By.xpath(xpath));
+			elements = BrowserManager.getDriver().findElements(
+				By.xpath(xpath));
 			count++;
 		}
 		
-		if(elements.size()>0)
-		{
-			LogManager.info(iconName + " is in the page " + count + " of icons"); 
+		if(elements.size()>0){
+			LogManager.info(iconName 
+					+ " is in the page " 
+					+ count 
+					+ " of icons"); 
 			return elements.get(0);
 		}
 		return null;
 		
 	}
 	
+	/**
+	 * This function is to selects on RECOURCE ASSOCIATION option
+	 * @return ResourceAssociationsPage
+	 */
 	public ResourceAssociationsPage clickOnResourcesAssociation()
 	{
-	   ExplicitWait.clickWhenReady(By.linkText(ResourceInfoMap.RESOURCES_ASSOCIATIONS_LINK), 5);
+	   ExplicitWait.clickWhenReady(By.linkText(
+		   ResourceInfoMap.RESOURCES_ASSOCIATIONS_LINK), 5);
 	   return new ResourceAssociationsPage();	
 	}
 	
