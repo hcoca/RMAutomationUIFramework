45eaad4a278915a071356cc36730717729d21444
belenluz <Lucero.Penarrieta@fundacion-jala.org>
2016-05-17 16:44:26 -0400

    Fix Framework: ConnectionMap

--- Projects/RMAutomationUIFramework/src/main/java/org/fundacionjala/automation/framework/maps/tablet/settings/ConnectionMap.java
+++ Projects/RMAutomationUIFramework/src/main/java/org/fundacionjala/automation/framework/maps/tablet/settings/ConnectionMap.java
@@ -4,4 +4,5 @@ public class ConnectionMap {
 
 	public static final String SERVICE_URL_TEXT_FIELD = "//input[@id='service-url-input'][@type='text'][@name='serviceURL']";
 	public static final String SAVE_BUTTON = "//span[text()='Save']/parent::button[@class='clean item item-btn'][@ng-click='saveConnectionSettings()']";
+	public static final String MESSAGE_STATUS = "//div[@ng-switch-default]";
 }
