1a26d2a0ccfce70e37af1017b07af3c5a471070e
belenluz <Lucero.Penarrieta@fundacion-jala.org>
2016-05-17 10:30:55 -0400

    Init Scenario: settings.feature-Steps Settings

---
+++ Projects/RMAutomationUIFramework/src/test/java/org/fundacionjala/automation/scenario/steps/tablet/settings/SettingsGivenSteps.java
@@ -0,0 +1,11 @@
+package org.fundacionjala.automation.scenario.steps.tablet.settings;
+
+import cucumber.api.java.en.Given;
+
+public class SettingsGivenSteps {
+    @Given("^I insert the Service URL parameter on Connection-Tablet page$")
+    public void i_insert_the_Service_URL_parameter_on_Connection_Tablet_page() throws Throwable {
+        
+    }
+
+}
