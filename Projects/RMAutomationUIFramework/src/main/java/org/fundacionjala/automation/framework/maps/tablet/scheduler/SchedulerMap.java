341e3655e30b06837f0416f1e4af3ffb1359f5f8
Rodrigo Cespedes <rodri.tronick@gmail.com>
2016-05-18 12:50:20 -0400

    Fix Scenario: A new meeting is displayed on timeline when a new meeting is created

--- Projects/RMAutomationUIFramework/src/main/java/org/fundacionjala/automation/framework/maps/tablet/scheduler/SchedulerMap.java
+++ Projects/RMAutomationUIFramework/src/main/java/org/fundacionjala/automation/framework/maps/tablet/scheduler/SchedulerMap.java
@@ -5,6 +5,7 @@ public class SchedulerMap {
 	public static final String ORGANIZER_TEXT_FIELD = "//input[@id='txtOrganizer'][@placeholder='User name'][@type='text']";
 	public static final String SUBJECT_TEXT_FIELD = "//input[@id='txtSubject'][@type='text'][@ng-model='editable.title']";
 	public static final String ATTENDEES_TEXT_FIELD = "//input[@type='text'][@ng-model='buffer'][@placeholder='Press enter or semicolon to confirm']";
+	public static final String BODY_TEXT_FIELD = "//textarea[@id='txtBody']";
 	public static final String CREATE_BUTTON = "//button[@class='clean item item-btn'][@ng-show='state.edit === editStates.CREATE']";
 	public static final String UPDATE_BUTTON = "//span[text()='Update']/parent::button[@class='clean item item-btn'][@ng-show='state.edit === editStates.UPDATE']";
 	public static final String REMOVE_BUTTON = "//span[text()='Remove']/parent::button[@class='clean item item-btn'][@ng-show='state.edit === editStates.UPDATE']";
