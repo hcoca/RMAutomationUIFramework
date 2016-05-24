b79375d910129d380da1ffb42778e2ee23f81bcb
belenluz <Lucero.Penarrieta@fundacion-jala.org>
2016-05-24 16:39:06 -0400

    Fix Framework: LocationPage

--- Projects/RMAutomationUIFramework/src/main/java/org/fundacionjala/automation/framework/pages/admin/locations/LocationPage.java
+++ Projects/RMAutomationUIFramework/src/main/java/org/fundacionjala/automation/framework/pages/admin/locations/LocationPage.java
@@ -3,8 +3,10 @@ package org.fundacionjala.automation.framework.pages.admin.locations;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.fundacionjala.automation.framework.maps.admin.conferencerooms.ConferenceRoomsMap;
 import org.fundacionjala.automation.framework.maps.admin.locations.LocationMap;
 import org.fundacionjala.automation.framework.maps.admin.resource.ResourceMap;
+import org.fundacionjala.automation.framework.pages.admin.conferencerooms.ConferenceRoomsPage;
 import org.fundacionjala.automation.framework.pages.admin.home.AdminPage;
 import org.fundacionjala.automation.framework.utils.common.BrowserManager;
 import org.fundacionjala.automation.framework.utils.common.ExplicitWait;
@@ -29,6 +31,10 @@ public class LocationPage extends AdminPage {
     WebElement addButton;
     @FindBy(xpath = LocationMap.REMOVE_LOCATION_BUTTON)
     WebElement removeButton;
+    @FindBy(xpath = LocationMap.NEXT_PAGE_FIELD)
+    WebElement pageField;
+    @FindBy(xpath = LocationMap.FIRST_ROW)
+    WebElement firstRow;
 
     /**
      * The constructor initializes web factory.
@@ -190,4 +196,14 @@ public class LocationPage extends AdminPage {
 		return ExplicitWait.getElementsWhenVisible(
 			By.xpath("//div[@tabindex]/div/div/child::*[3]//span"), 60);
 	    }
+
+    public LocationPage setPage(String page) {
+		pageField.clear();
+		pageField.sendKeys(page);
+		return this;
+    }
+    
+    public String getFirstRow() {
+    	return firstRow.getText();
+    }
 }
